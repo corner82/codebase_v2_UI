417b8be822cc4333cdbc8a43c7dbb7db823b658a
csyrek <ceydacanseyrek@gmail.com>
2018-07-09 10:31:24 +0300

    CSI CXI kadran başlıkları düzeltme

--- public/jsinline/managerdirector_index.js
+++ public/jsinline/managerdirector_index.js
@@ -12563,7 +12563,8 @@ function getVerimlilikDashboard() {
         height: 200
     },
     title: {
-        text: window.lang.translate('Productivity')
+        //text: window.lang.translate('Productivity')
+        text: 'CSI'
     },
     pane: [{
         startAngle: -45,
@@ -12913,7 +12914,8 @@ function getKapasiteDashboard() {
         height: 200
     },
     title: {
-        text: ''+window.lang.translate('Capacity utilization')+''
+        //text: ''+window.lang.translate('Capacity utilization')+''
+        text: 'CXI'
     },
     pane: [{
         startAngle: -45,
