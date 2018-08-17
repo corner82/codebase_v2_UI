34a8edba73ae6a536887f662c2e672521cbf6de4
corner82 <311corner82@gmail.com>
2018-08-17 16:56:22 +0300

    tab organizer, panel slider left / right widget'lar eklendi

--- public/plugins/zeynel/exPlugs.js
+++ public/plugins/zeynel/exPlugs.js
@@ -2434,6 +2434,362 @@
 
     });
     
+    /**
+     * navigate, active, passive bootstrap 3 tabs
+     * @author Mustafa Zeynel Dağlı
+     * @since 13/08/2018
+     */
+    $.widget("sanalfabrika.organizeTabs", {
+        /**
+         * Default options.
+         * @returns {null}
+         */
+        options: {
+            tabID : "domTabID",
+
+        },
+        /**
+         * private constructor method for jquery widget
+         * @returns {null}
+         */
+        _create: function () {
+            var self = this;
+            $('#'+self.options.tabID+' a').click(function (e) {
+                //alert('click tag');
+                if ($(this).hasClass("disabled")) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    e.stopImmediatePropagation();
+                    return false;
+
+                }
+            }); 
+        },
+
+        /**
+         * activate next tab without tab active/passive control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        activateNextTab: function () {
+            var self = this;
+            $('#' + self.options.tabID + ' li.active').removeClass("active").find('a:first').attr("aria-expanded", "false").parent("li").next('li').addClass("active").find("a:first").attr("aria-expanded", "true");
+            $('#' + self.options.tabID + ' li.active').removeClass("active").find('a:first').tab('show');
+            /*if ($('#' + self.options.tabID + ' li.active').next('li').find('a:first').hasClass('disabled')) {
+                console.log('tab to be activated has been disabled before');
+            } else {
+                $('#'+self.options.tabID+' li.active').removeClass("active").find('a:first').attr("aria-expanded", "false").parent("li").next('li').addClass("active").find("a:first").attr("aria-expanded", "true");
+                $('#'+self.options.tabID+' li.active').removeClass("active").find('a:first').tab('show');
+            }*/
+        },
+
+        /**
+         * activate prev tab without tab active/passive control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        activatePrevTab: function () {
+            var self = this;           
+            $('#' + self.options.tabID + ' li.active').removeClass("active").find('a:first').attr("aria-expanded", "false").parent("li").prev('li').addClass("active").find("a:first").attr("aria-expanded", "true");
+            $('#' + self.options.tabID + ' li.active').removeClass("active").find('a:first').tab('show');
+            /*if ($('#' + self.options.tabID + ' li.active').prev('li').find('a:first').hasClass('disabled')) {
+                console.log('tab to be activated has been disabled before');
+            } else {
+                $('#' + self.options.tabID + ' li.active').removeClass("active").find('a:first').attr("aria-expanded", "false").parent("li").prev('li').addClass("active").find("a:first").attr("aria-expanded", "true");
+                $('#' + self.options.tabID + ' li.active').removeClass("active").find('a:first').tab('show');
+            }*/
+        },
+
+        /**
+         * activate tab by order without tab active/passive control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        activateTabByOrder: function (order) {
+            var self = this;
+            $('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').removeClass("active").find('a:first').attr("aria-expanded", "false").parent("li").next('li').addClass("active").find("a:first").attr("aria-expanded", "true");
+            $('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').removeClass("active").find('a:first').tab('show');
+            /*if ($('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').find('a:first').hasClass('disabled')) {
+                console.log('tab to be activated has been disabled before');
+            } else {
+                alert('next tab active order 2 ');
+                $('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').removeClass("active").find('a:first').attr("aria-expanded", "false").parent("li").next('li').addClass("active").find("a:first").attr("aria-expanded", "true");
+                $('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').removeClass("active").find('a:first').tab('show');
+            }*/
+        },
+
+        /**
+         * disable all tabs without tab disable/enable control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        disableAllTabs: function () {
+            var self = this;
+            $('#' + self.options.tabID + '  li').find("a:first").addClass('disabled');
+            $('#' + self.options.tabID + '  li').find("a:first").removeAttr('data-toggle');
+        },
+
+        /**
+         * disable all tabs but the one tab is active
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        disableAllTabsButOne: function () {
+            var self = this;
+            $('#' + self.options.tabID + '  li').not('.active').find("a:first").addClass('disabled');
+            $('#' + self.options.tabID + '  li').not('.active').find("a:first").removeAttr('data-toggle');
+        },
+
+        /**
+         * disable tab by order without tab disable/enable control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        disableTabByOrder: function (order) {
+            var self = this;
+            $('#' + self.options.tabID + '  li:eq(' + parseInt(order) + ')').find("a:first").addClass('disabled');
+            $('#' + self.options.tabID + '  li:eq(' + parseInt(order) + ')').find("a:first").removeAttr('data-toggle');
+        },
+
+        /**
+         * enable all tabs  without tab disable/enable control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        enableAllTabs: function () {
+            var self = this;
+            $('#' + self.options.tabID + ' li').not('.active').find("a.dropdown-toggle.disabled").removeClass('disabled').attr("data-toggle", "dropdown");
+            $('#' + self.options.tabID + ' li').not('.active').find("a.disabled").removeClass('disabled').attr("data-toggle", "tab");
+        },
+
+        /**
+         * enable tab by order without tab disable/enable control
+         * @author Mustafa Zeynel Dağlı
+         * @since 14/08/2018
+         */
+        enableTabByOrder: function (order) {
+            var self = this;
+            $('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').not('.active').find("a.dropdown-toggle.disabled").removeClass('disabled').attr("data-toggle", "dropdown");
+            $('#' + self.options.tabID + ' li:eq(' + parseInt(order) + ')').not('.active').find("a.disabled").removeClass('disabled').attr("data-toggle", "tab");
+        },
+
+
+    });
+    
+    /**
+     * create custom left aside widget
+     * @author Mustafa Zeynel Dağlı
+     * @since 15/08/2018
+     */
+    $.widget("sanalfabrika.asideLeft", {
+        /**
+         * Default options.
+         * @returns {null}
+         */
+        options: {
+            width: 700,
+            slide: true,
+            top: 0,
+            bottom: 0,
+        },
+        /**
+         * private constructor method for jquery widget
+         * @returns {null}
+         */
+        _create: function () {
+            var self = this;
+
+            /*(function (a) {
+                (jQuery.browser = jQuery.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))
+            })(navigator.userAgent || navigator.vendor || window.opera);*/
+
+            //alert($(window).width());
+            if ($(window).width() < self.options.width) {
+                //alert("mobile");
+                self.options.width = $(window).width();
+            }
+            else {
+                //alert("not mobile");
+            }
+            self.element.css('left', '-' + self.options.width + 'px');
+            self.element.css('width', '' + self.options.width + 'px');
+            self.element.css('top', '0px');
+            self.element.addClass('control-sidebar-custom-left');
+            self.element.addClass('control-sidebar-custom-light');
+            
+        },
+
+        /**
+         * open aside
+         * @author Mustafa Zeynel Dağlı
+         * @since 15/08/2018
+         */
+        open: function () {
+            var self = this;
+            if (self.options.slide) {
+                self._trigger('onOpening', event, self.element);
+
+                self.element.css('left', '0');
+                self.element.addClass('control-sidebar-custom-open');
+
+                self._trigger('onOpened', event, { data: self.element });
+                
+            } else {
+            }
+        },
+
+        /**
+         * close aside
+         * @author Mustafa Zeynel Dağlı
+         * @since 16/08/2018
+         */
+        close: function () {
+            var self = this;
+            if (self.options.slide) {
+                self._trigger('onClosing', event, { data: self.element });
+
+                self.element.css('left', '-' + self.options.width + 'px');
+                self.element.removeClass('control-sidebar-custom-open');
+
+                self._trigger('onClosed', event, { data: self.element });
+            } else {
+            }
+        },
+
+        /**
+        * toggle open/close aside
+        * @author Mustafa Zeynel Dağlı
+        * @since 15/08/2018
+        */
+        toggle: function () {
+            var self = this;
+            if (self.element.hasClass('control-sidebar-custom-open')) {
+                self._trigger('onClosing', event, { data: self.element });
+
+                self.element.css('left', '-' + self.options.width + 'px');
+                self.element.removeClass('control-sidebar-custom-open');
+
+                self._trigger('onClosed', event, { data: self.element });
+            } else {
+                self._trigger('onOpening', event,  self.element );
+
+                self.element.css('left', '0px');
+                self.element.addClass('control-sidebar-custom-open');
+
+                self._trigger('onOpened', event, { data: self.element });
+            }
+        },
+
+    });
+    
+    /**
+    * create custom right aside widget
+    * @author Mustafa Zeynel Dağlı
+    * @since 15/08/2018
+    */
+    $.widget("sanalfabrika.asideRight", {
+        /**
+         * Default options.
+         * @returns {null}
+         */
+        options: {
+            width: 700,
+            slide: true,
+            top: 0,
+            bottom: 0,
+        },
+        /**
+         * private constructor method for jquery widget
+         * @returns {null}
+         */
+        _create: function () {
+            var self = this;
+
+            /*(function (a) {
+                (jQuery.browser = jQuery.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))
+            })(navigator.userAgent || navigator.vendor || window.opera);*/
+
+            //alert($(window).width());
+            if ($(window).width() < self.options.width) {
+                //alert("mobile");
+                self.options.width = $(window).width();
+            }
+            else {
+                //alert("not mobile");
+            }
+            self.element.css('right', '-' + self.options.width + 'px');
+            self.element.css('width', '' + self.options.width + 'px');
+            self.element.css('top', '0px');
+            self.element.addClass('control-sidebar-custom-right');
+            self.element.addClass('control-sidebar-custom-light');
+
+        },
+
+        /**
+         * open aside
+         * @author Mustafa Zeynel Dağlı
+         * @since 15/08/2018
+         */
+        open: function () {
+            var self = this;
+            if (self.options.slide) {
+                self._trigger('onOpening', event, self.element);
+
+                self.element.css('right', '0');
+                self.element.addClass('control-sidebar-custom-open');
+
+                self._trigger('onOpened', event, { data: self.element });
+
+            } else {
+            }
+        },
+
+        /**
+         * close aside
+         * @author Mustafa Zeynel Dağlı
+         * @since 16/08/2018
+         */
+        close: function () {
+            var self = this;
+            if (self.options.slide) {
+                self._trigger('onClosing', event, { data: self.element });
+
+                self.element.css('right', '-' + self.options.width + 'px');
+                self.element.removeClass('control-sidebar-custom-open');
+
+                self._trigger('onClosed', event, { data: self.element });
+            } else {
+                
+            }
+        },
+
+        /**
+        * toggle open/close aside
+        * @author Mustafa Zeynel Dağlı
+        * @since 15/08/2018
+        */
+        toggle: function () {
+            var self = this;
+            if (self.element.hasClass('control-sidebar-custom-open')) {
+                self._trigger('onClosing', event, { data: self.element });
+
+                self.element.css('right', '-' + self.options.width + 'px');
+                self.element.removeClass('control-sidebar-custom-open');
+
+                self._trigger('onClosed', event, { data : self.element });
+
+            } else {
+                self._trigger('onOpening', event, { data: self.element });
+
+                self.element.css('right', '0px');
+                self.element.addClass('control-sidebar-custom-open');
+
+                self._trigger('onOpened', event, { data: self.element });
+            }
+        },
+
+    });
+    
     
 
 }(jQuery));
